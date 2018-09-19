322e3b52e457cfb940026587f1b31f179c4749e5
Howard <ho1919192000@gmail.com>
2018-09-18 21:30:12 -0600

    update getters and setters

--- class.js
+++ class.js
@@ -110,3 +110,26 @@ const carrot = new Vegetable('carrot');
 console.log(carrot.name); // => should be 'carrot'
 
 /*ES6: Use getters and setters to Control Access to an Object*/
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+  class Thermostat {
+    constructor(f){
+      this.c = 5/9 * (f-32);
+    }
+    get temperature() {
+      return this.c;
+    }
+
+    set temperature(c) {
+      this.c = c;
+    }
+  }
+  /* Alter code above this line */
+  return Thermostat;
+}
+const Thermostat = makeClass();
+const thermos = new Thermostat(76); // setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in C
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in C
